51b91fc1509bf9b5dd31c7ca5f0e4c6d0e893a24
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-02-23 23:21:49 +0300

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    div = 5;
+    zeros_amount = 0;
+    while (number >= div) {
+        zeros_amount += Math.floor(number / div);
+        div = div * 5;
+    }
+    return zeros_amount
 }
